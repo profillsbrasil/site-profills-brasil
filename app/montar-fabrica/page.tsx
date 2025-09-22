adf72c8706ac0d0c4e555924f8c1676bd01b1036
Othavio Quiliao <othavioaqa@gmail.com>
2025-09-22 15:33:11 -0300

    feat: enhance UI with image integration and layout adjustments

--- app/montar-fabrica/page.tsx
+++ app/montar-fabrica/page.tsx
@@ -1,7 +1,19 @@
+import imgFabricaCompleta from "@/lib/images/extras/FabricaCompleta.png";
+import Image from "next/image";
+
 export default function MonteSuaFabrica() {
   return (
-    <div>
-      <h1>Monte sua fábrica</h1>
+    <div className="relative flex h-[90vh] min-h-[90vh] w-full items-center justify-center pt-10">
+      <div className="h-full w-full">
+        <Image
+          src={imgFabricaCompleta}
+          alt="Monte sua fábrica"
+          className="h-full w-full object-contain"
+        />
+      </div>
+      <div className="absolute top-0 right-0 h-full w-1/4">
+        <h1>Monte sua fábrica</h1>
+      </div>
     </div>
   );
 }
