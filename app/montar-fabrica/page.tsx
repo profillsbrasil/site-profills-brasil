5227eac9fb0af0867dd71da946216a8ff38cf26d
Othavio Quiliao <othavioaqa@gmail.com>
2025-09-23 11:39:54 -0300

    feat: improve layout and enhance title styling in MonteSuaFabrica component

--- app/montar-fabrica/page.tsx
+++ app/montar-fabrica/page.tsx
@@ -1,19 +1,26 @@
+"use client";
+
+import { GridPattern } from "@/components/layout/gridPatternBg";
 import imgFabricaCompleta from "@/lib/images/extras/FabricaCompleta.png";
 import Image from "next/image";
 
 export default function MonteSuaFabrica() {
   return (
-    <div className="relative flex h-[90vh] min-h-[90vh] w-full items-center justify-center pt-10">
-      <div className="h-full w-full">
+    <div className="relative flex h-screen min-h-screen w-full items-center justify-start bg-slate-900">
+      <GridPattern />
+
+      <div className="z-20 h-full w-4/6">
         <Image
           src={imgFabricaCompleta}
           alt="Monte sua fábrica"
           className="h-full w-full object-contain"
         />
       </div>
-      <div className="absolute top-0 right-0 h-full w-1/4">
-        <h1>Monte sua fábrica</h1>
-      </div>
+
+      {/* Título principal melhorado */}
+      <h1 className="absolute top-28 left-1/2 z-10 w-full -translate-x-1/2 text-center text-5xl font-bold text-white uppercase">
+        <span className="w-full">Monte sua Fábrica</span>
+      </h1>
     </div>
   );
 }
