4f04dca8984c6754b841e7c9a5650237f559de43
Othavio Quiliao <othavioaqa@gmail.com>
2026-05-13 14:16:16 -0300

    fix(layout): html relative p/ motion useScroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='pt-BR' suppressHydrationWarning>
+    <html lang='pt-BR' suppressHydrationWarning className='relative'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} h-full w-full overflow-x-hidden antialiased`}
         suppressHydrationWarning>
